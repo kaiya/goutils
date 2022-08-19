78d6044aa195d00dbbcaa30881280d9c4dfb464e
Kaiya <kaiya.xiong337@imaygou.com>
2022-08-19 14:51:29 +0800

    add utils

--- io/file/parse.go
+++ io/file/parse.go
@@ -20,3 +20,38 @@ func ParseJsonFile(path string, out interface{}) error {
 	}
 	return json.Unmarshal(bytes, out)
 }
+
+// json format {"data": ["a","b"]}
+func DiffJsonFiles(allPath, setPath string) ([]string, error) {
+	setDocs := struct {
+		Data []string `json:"data"`
+	}{}
+	err := ParseJsonFile(setPath, &setDocs)
+	if err != nil {
+		return nil, err
+	}
+	allDocs := struct {
+		Data []string `json:"data"`
+	}{}
+	err = ParseJsonFile(allPath, &allDocs)
+	if err != nil {
+		return nil, err
+	}
+	return DiffSlices(allDocs.Data, setDocs.Data), nil
+}
+
+// get not exists of all in set
+// TODO replace in place
+func DiffSlices(all, set []string) []string {
+	setMap := make(map[string]struct{})
+	for _, s := range set {
+		setMap[s] = struct{}{}
+	}
+	ret := make([]string, 0)
+	for _, a := range all {
+		if _, ok := setMap[a]; !ok {
+			ret = append(ret, a)
+		}
+	}
+	return ret
+}
